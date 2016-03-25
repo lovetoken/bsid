0eebdf2894fb32518661c3951c5ccfb9d5ef3486
lovetoken <lt.lovetoken@gmail.com>
2016-03-25 16:51:42 +0900

    0.132 version commit  - add "MDMR_filter" denoising method  - change image denoising method of README.Rmd

--- R/denoising.r
+++ R/denoising.r
@@ -7,11 +7,11 @@
 #' @export
 #' @examples
 #'
-denoising <- function(x, method=c("mean", "median", "MDMR", "cross_shape_mean", "cross_shape_median", "cross_shape_MDMR", "mean_oneframe", "median_oneframe", "auto_outlier_filter", "median_filter"),
+denoising <- function(x, method=c("mean", "median", "MDMR", "cross_shape_mean", "cross_shape_median", "cross_shape_MDMR", "mean_oneframe", "median_oneframe", "auto_outlier_filter", "median_filter", "MDMR_filter"),
                       W0, lambda, save.env="bsid_env", output.naming=NULL, ...){
   # pre
   stopifnot(require(dplyr)); stopifnot(require(progress))
-  stopifnot(method %in% c("mean", "median", "MDMR", "cross_shape_mean", "cross_shape_median", "cross_shape_MDMR", "mean_oneframe", "median_oneframe", "auto_outlier_filter", "median_filter"))
+  stopifnot(method %in% c("mean", "median", "MDMR", "cross_shape_mean", "cross_shape_median", "cross_shape_MDMR", "mean_oneframe", "median_oneframe", "auto_outlier_filter", "median_filter", "MDMR_filter"))
   stopifnot(is(output.naming, "character") | is.null(output.naming))
   if(!save.env %in% ls(globalenv())) assign(save.env, new.env(), envir=globalenv())
   n1 <- attributes(x)$n1
@@ -298,6 +298,44 @@ denoising <- function(x, method=c("mean", "median", "MDMR", "cross_shape_mean", 
 
         prime_for_prime[i, j] <- median(star[matrix.scale, window(W0, j, 1, n2)])
         Mdf_for_prime[i, j] <- abs(median(star[matrix.scale, window(W0, j, 1, n2)])-star[i,j])
+
+      }
+
+    }
+
+    Mnoise_detect <- apply(Mdf_for_prime, 2, function(x) x>=quantile(x, probs=1-lambda))
+
+    for(i in seq(n1)){
+      for(j in seq(n2)){
+        if(Mnoise_detect[i,j]==TRUE){
+          prime[i,j] <- prime_for_prime[i, j]
+        } else {
+          next
+        }
+      }
+
+    }
+
+  } else if(method=="MDMR_filter"){
+
+    # Mean detect and Median replace filter method denoising algorithm
+    Mdf_for_prime <- prime_for_prime <- matrix(NA, n1, n2)
+    pb <- progress_bar$new(total=n1)
+    for(i in seq(n1)){
+
+      pb$tick()
+      # ith row Mmatrix scale >> real picture scale
+      h <- i%%attributes(x)$height
+      w <- (i%/%attributes(x)$height)+1
+
+      # real picture center scale >> return to detect spatial range >> Mmatix scaling
+      frame.scale <- expand.grid(h.detect=window(W0, h, 1, attributes(x)$height), w.detect=window(W0, w, 1, attributes(x)$width))
+      matrix.scale <- frame.scale$h.detect+attributes(x)$height*(frame.scale$w.detect-1)
+
+      for(j in seq(n2)){
+
+        prime_for_prime[i, j] <- median(star[matrix.scale, window(W0, j, 1, n2)])
+        Mdf_for_prime[i, j] <- abs(mean(star[matrix.scale, window(W0, j, 1, n2)])-star[i,j])
 
       }
 
