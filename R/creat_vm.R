1a5ef2e3488d7cfb922a5ad028aedabf08e5a948
lovetoken <lt.lovetoken@gmail.com>
2016-04-19 15:36:42 +0900

    0.134 version commit  - creat_vm() function 에서 jpg, jpeg 형식도 지원

--- R/creat_vm.R
+++ R/creat_vm.R
@@ -1,6 +1,6 @@
 #' Create vedio matrix
 #'
-#' 순차적으로 저장되어있는 영상 프레임 (.bmp format 만 현재 지원) 워킹디렉토리를 지정시 vedio matrix 를 만들어 반환합니다.
+#' 순차적으로 저장되어있는 영상 프레임 (bmp, jpg, jpeg format 만 현재 지원) 워킹디렉토리를 지정시 vedio matrix 를 만들어 반환합니다.
 #' 반환되는 객체는 vm class로 본 내용물은 행렬이며, 원본 프레임의 메타정보 들은(e.g n1, n2) 속성값으로 저장됩니다.
 #' @param wd 영상프레임 집합의 path 를 vector 형식으로 입력합니다.
 #' @param output.naming 이 인자에 값을 부여하면 반환되는 vedio matrix 를 .Rdata 형식파일을 외부로 저장시킵니다.
@@ -11,23 +11,25 @@
 #' @export
 #' @examples
 
-creat_vm <- function(wd=dir(full.names=TRUE, ...), rgb=FALSE, save.env="bsid_env", output.naming=NULL, ...){
+creat_vm <- function(wd=dir(full.names=TRUE, ...), rgb=FALSE, save.env="bsid_env", output.naming=NULL, format="bmp", ...){
   # pre
-  stopifnot(require(bmp)); stopifnot(require(pixmap)); stopifnot(require(progress))
+  stopifnot(require(bmp)); stopifnot(require(pixmap)); stopifnot(require(progress)); stopifnot(require(jpeg))
   stopifnot(is(output.naming, "character") | is.null(output.naming))
+  stopifnot(is(format, "character"))
   stopifnot(is(wd, "vector"))
   wd <- sort(wd)
   if(!save.env %in% ls(globalenv())) assign(save.env, new.env(), envir=globalenv())
 
   # content
-  if(rgb==TRUE){ # rgb 구현중
+
+  # if(rgb==TRUE){ # rgb 구현중
     # filename <- filename.fun(dir, main.name, seq.start, seq.end)
     #
     # # width & height value setting
-    # tmp.readbmp <- read.bmp(filename[1])
+    # read <- read.bmp(filename[1])
     #
-    # width <- attributes(tmp.readbmp)$header$width
-    # height <- attributes(tmp.readbmp)$header$height
+    # width <- attributes(read)$header$width
+    # height <- attributes(read)$header$height
     #
     # # M matrix space
     # MR <- matrix(NA, nrow=width*height, ncol=length(filename))
@@ -47,18 +49,24 @@ creat_vm <- function(wd=dir(full.names=TRUE, ...), rgb=FALSE, save.env="bsid_env
     # n1 <- width*height; n2 <- length(filename)
     #
     # save(MR, MG, MB, n1, n2, width, height, file=paste0(output.naming, "_", main.name, "_", seq.start, "~", seq.end, "_RGB.Rdata"))
-  } else {
+  # }
 
-    # width & height value setting
-    tmp.readbmp <- read.bmp(wd[1])
+  ## width & height value setting
+  if(format=="bmp"){
+    read <- read.bmp(wd[1])
+  } else if(format %in% c("jpg", "jpeg")){
+    read <- readJPEG(wd[1])
+  }
 
-    width <- attributes(tmp.readbmp)$header$width
-    height <- attributes(tmp.readbmp)$header$height
+  width <- dim(read)[1]
+  height <- dim(read)[2]
 
-    # M matrix space
-    M <- matrix(NA, nrow=width*height, ncol=length(wd))
+  ## M matrix space
+  M <- matrix(NA, nrow=width*height, ncol=length(wd))
 
-    # M matrix space <- grey scale pixel value
+  ## M matrix space <- grey scale pixel value
+
+  if(format=="bmp"){
 
     pb <- progress_bar$new(total=length(wd))
     for(i in seq(length(wd))){
@@ -68,13 +76,23 @@ creat_vm <- function(wd=dir(full.names=TRUE, ...), rgb=FALSE, save.env="bsid_env
       M[,i] <- as.numeric(attributes(grey.scale)$grey)
     }
 
-    # attributes of `M`
-    attr(M, "width") <- width;
-    attr(M, "height") <- height
-    attr(M, "n1") <- width*height
-    attr(M, "n2") <- length(wd)
+  } else if(format %in% c("jpg", "jpeg")){
+
+    pb <- progress_bar$new(total=length(wd))
+    for(i in seq(length(wd))){
+      pb$tick()
+      readjp <- readJPEG(f=wd[i])
+      grey.scale <- pixmapGrey(readjp)
+      M[,i] <- as.numeric(attributes(grey.scale)$grey)
+    }
 
   }
+
+  ## attributes of `M`
+  attr(M, "width") <- width;
+  attr(M, "height") <- height
+  attr(M, "n1") <- width*height
+  attr(M, "n2") <- length(wd)
 
   # return
   assign("M", M, envir=get(save.env))
